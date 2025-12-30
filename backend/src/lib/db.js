3e09b2e2af74f0e578fb10d8b1df134393c4480b
ashiqaj <ashiqjmohammed@gmail.com>
2025-12-30 20:59:31 +0530

    login&logout completed

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDb = async()=> {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB connected sccessfully");
     }
     catch(error) {
